15fe57c0d8af067f09c25c65c76d3c15cdc8a345
Rakibul Islam <rakib.ca@gmail.com>
2025-09-23 23:47:35 -0600

    code update

---
+++ 73_Optional Chaining.js
@@ -0,0 +1,45 @@
+const library = {
+  sections: {
+    fiction: {
+      genres: {
+        fantasy: [
+          { title: 'The Hobbit', author: 'J.R.R. Tolkien', year: 1937 },
+          {
+            title: 'A Game of Thrones',
+            author: 'George R.R. Martin',
+            year: 1996,
+          },
+        ],
+        scienceFiction: [
+          { title: 'Dune', author: 'Frank Herbert', year: 1965 },
+          { title: 'Neuromancer', author: 'William Gibson', year: 1984 },
+        ],
+      },
+    },
+  },
+};
+
+console.log(library.sections.fiction.genres.fantasy[0].year);
+console.log(library && library.sections);
+
+/*
+Challenge A:
+    1. Use the logical AND operator to log The Hobit object. 
+       Notice how undefined is returned if you break the chain. 
+*/
+// SEE ANSWER BELOW //
+console.log(
+  library &&
+    library.sections &&
+    library.sections.fiction &&
+    library.sections.fiction.genres &&
+    library.sections.fiction.genres.fantasy[0]
+);
+
+/*
+Challenge B:
+    1. Apply optional chaining along the line to 
+       log out The Hobit's year.
+*/
+// SEE ANSWER BELOW //
+console.log(library?.sections?.fiction?.genres?.fantasy[0]?.year);
